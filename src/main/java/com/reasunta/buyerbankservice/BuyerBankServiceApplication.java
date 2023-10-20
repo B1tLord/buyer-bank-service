ac658397b12135c1dd2103bf1e815601d8d80d3f
Stanislav Medvedev <bitlord@xaker.ru>
2023-10-20 12:02:18 +0200

    Initial commit with Spring Boot scaffold

---
+++ src/main/java/com/reasunta/buyerbankservice/BuyerBankServiceApplication.java
@@ -0,0 +1,13 @@
+package com.reasunta.buyerbankservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyerBankServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyerBankServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reasunta/buyerbankservice/BuyerBankServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reasunta.buyerbankservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyerBankServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
